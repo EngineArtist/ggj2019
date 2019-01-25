3f3cbc9dc9a387b88535a6cdeb43647931429434
vodrilus <salotuomas@gmail.com>
2019-01-25 22:36:40 +0200

    Added basic classes for game events

---
+++ Assets/Scripts/EventMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class EventMaster : MonoBehaviour
+{
+    public GameEvent currentEvent = null;
+    public List<GameEvent> allEvents;
+    public System.Random rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.allEvents = new List<GameEvent>();
+        this.rand = new System.Random();
+    }
+
+    GameEvent GetRandomEvent()
+    {
+        int index = this.rand.Next(allEvents.Count());
+        return this.allEvents[index];
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameEvent
+{
+    public string mainMessage;
+    public List<EventOption> options;
+
+    public GameEvent(string mainMessage)
+    {
+        this.mainMessage = mainMessage;
+        this.options = new List<EventOption>();
+    }
+    
+}
+
+public class EventOption
+{
+    public string initialText;
+    public int stress;
+    // TODO: add result types
+    public EventOption(string initialText, string resultText, int stress = 0)
+    {
+        this.initialText = initialText;
+        this.stress = stress;
+    }
+}
