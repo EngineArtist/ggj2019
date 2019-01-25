4d6185ae91c6d27d28f0c0b4eb8c362d6fdb2b75
Teemu Daniel Laajala <teemu.daniel.laajala@gmail.com>
2019-01-25 21:27:03 +0200

    Script base

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    // Whether the spaceship animation / flying is active
+    private bool progressActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.progressActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
