5901683403d4c1f5cd893c267a17273204a27aef
Teemu Daniel Laajala <teemu.daniel.laajala@gmail.com>
2019-01-25 23:40:10 +0200

    Generalizated lists to add flexibility for various objects

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -11,41 +11,47 @@ public class GameMaster : MonoBehaviour
     // General HUD component
     public GameObject HUDcomponent;
     // Specific HUD components that are updated on the run
-    private GameObject HUDhull;
-    private GameObject HUDenergy;
-    private GameObject HUDpopulation;
+    public Dictionary<string, GameObject> HUDobjects;
     // Values for the resources
-    private double hull;
-    private int energy;
-    private int population;
+    public Dictionary<string, int> resources;
+    // Objects in space
+    public List<GameObject> spaceObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         progressActive = true;
         CreateHUD();
-        hull = 100.0;
-        energy = 1;
-        population = 1;
+        // Set default values for the various resources
+        resources.Add("hull", 100);
+        resources.Add("energy", 1);
+        resources.Add("population", 1);
+        // Dynamically allocated array of objects in space
+        spaceObjects = new List<GameObject>();
     }
 
     // Create an interface for the player
     private void CreateHUD()
     {
+        GameObject tmp;
         // Create the player HUD
-        // Use viewport to position the HUD elements
+        // Use viewport to position the HUD elements; 
+        // {0,0} is lower-left corner, {1,1} is top-right and the 3rd component is the depth
         // Hull 
-        HUDhull = GameObject.Instantiate(this.HUDcomponent);
-        HUDhull.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 10));
-        HUDhull.GetComponent<TextMesh>().text = "Hull";
+        tmp = GameObject.Instantiate(this.HUDcomponent);
+        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 10));
+        tmp.GetComponent<TextMesh>().text = "Hull";
+        HUDobjects.Add("hull", tmp);
         // Energy
-        HUDenergy = GameObject.Instantiate(this.HUDcomponent);
-        HUDenergy.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.4f, 1, 10));
-        HUDenergy.GetComponent<TextMesh>().text = "Energy";
+        tmp = GameObject.Instantiate(this.HUDcomponent);
+        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.4f, 1, 10));
+        tmp.GetComponent<TextMesh>().text = "Energy";
+        HUDobjects.Add("energy", tmp);
         // Population
-        HUDenergy = GameObject.Instantiate(this.HUDcomponent);
-        HUDenergy.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.7f, 1, 10));
-        HUDenergy.GetComponent<TextMesh>().text = "Population";
+        tmp = GameObject.Instantiate(this.HUDcomponent);
+        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.7f, 1, 10));
+        tmp.GetComponent<TextMesh>().text = "Population";
+        HUDobjects.Add("population", tmp);
     }
 
     // Update is called once per frame
