88b4b46075858d5b6e2fd01b7d89f69ec3d7a9d7
Miika Vihersaari <miika.vihersaari@gmail.com>
2019-01-26 18:15:26 +0200

    Added ship hover effect

---
+++ Assets/Scripts/HoverSine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HoverSine: MonoBehaviour {
+
+    public float frequency;
+    public float amplitude;
+
+    Vector3 initPos;
+
+    void Start() {
+        initPos = transform.localPosition;
+    }
+
+    void Update() {
+        transform.localPosition = initPos + Vector3.up*Mathf.Sin(Time.time*frequency)*amplitude;
+    }
+}
