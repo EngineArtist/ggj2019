7bff4c592242adf5da4b8cf5838b690d8705c640
Teemu Daniel Laajala <teemu.daniel.laajala@gmail.com>
2019-01-26 17:25:47 +0200

    Event timers

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -21,7 +21,6 @@ public class GameMaster : MonoBehaviour
     // Objects in space
     public List<GameObject> spaceObjects;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -32,8 +31,11 @@ public class GameMaster : MonoBehaviour
         // Game is on by default
         progressActive = true;
         // Event trigger logic
-        vars.Add("timeToEvent", 10.0f);
+        vars.Add("baseTimeToEvent", 6.0f);
+        vars.Add("timeToEvent", 6.0f);
         vars.Add("eventSpeed", 1.0f);
+        vars.Add("difficulty", 1.0f);
+        vars.Add("distanceTraveled", 0.0f);
         // Create the HUD
         CreateHUD();
         // Set default values for the various resources
@@ -60,14 +62,18 @@ public class GameMaster : MonoBehaviour
         HUDobjects.Add("hull", tmp);
         // Energy
         tmp = GameObject.Instantiate(HUDcomponent);
-        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.4f, 1, 10));
+        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.3f, 1, 10));
         tmp.GetComponent<TextMesh>().text = "Energy";
         HUDobjects.Add("energy", tmp);
         // Population
         tmp = GameObject.Instantiate(HUDcomponent);
-        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.7f, 1, 10));
+        tmp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.6f, 1, 10));
         tmp.GetComponent<TextMesh>().text = "Population";
         HUDobjects.Add("population", tmp);
+        // Base HUD update
+        updateHUD("hull", 100.0f);
+        updateHUD("energy", 100.0f);
+        updateHUD("population", 100.0f);
         // HUD COMPONENTS INTENDED FOR DEBUGGING
         // Time until next event
         tmp = GameObject.Instantiate(HUDcomponent);
@@ -77,11 +83,18 @@ public class GameMaster : MonoBehaviour
     }
 
     // Update a text mesh displaying something on the HUD
-    private void updateHUD(string component, int newvalue)
+    private void updateHUD(string component, double newvalue)
     {
-
+        switch (component)
+        {
+            case "hull":
+                HUDobjects[component].GetComponent<TextMesh>().text = component + ": " + (int)newvalue + "%";
+                break;
+            default:
+                HUDobjects[component].GetComponent<TextMesh>().text = component + ": " + (int)newvalue;
+                break;
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -90,15 +103,18 @@ public class GameMaster : MonoBehaviour
         {
             // Logic for generating new events in real time
             vars["timeToEvent"] -= vars["eventSpeed"] * Time.deltaTime;
+            updateHUD("timertoevent", vars["timeToEvent"]);
             // Trigger an event
             if(vars["timeToEvent"] < 0) 
             {
                 // Event logic
-
+                TriggerTimeEvent();
                 // Reset time to next event
-
+                vars["timeToEvent"] = vars["baseTimeToEvent"];
             }
             // Logic for space progression
+            // Keep a counter for distance traveled
+            vars["distanceTraveled"] += vars["distanceTraveled"] * Time.deltaTime;
         } 
         // Otherwise in menus or in an event yet to be resolved
         else
@@ -108,6 +124,12 @@ public class GameMaster : MonoBehaviour
             // After event is resolved, resume gaming
 
         }
+    }
+
+    // Time triggered spontaneous events
+    private void TriggerTimeEvent()
+    {
+       
     }
 }
 
