6f21a1a0f102cac3fd370aab6e347bc3b153f5bb
Miika Vihersaari <miika.vihersaari@gmail.com>
2019-01-27 14:43:46 +0200

    Made an explosion

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    void Start() {
+        Destroy(gameObject, .9f);
+    }
+}
